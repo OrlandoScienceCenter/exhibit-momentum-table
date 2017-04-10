095cb7e125c3db5fa6f282c1150f5003bf3db982
Michael King <techguy.mking@gmail.com>
2017-04-10 14:55:58 -0400

    program flow layout

--- Exhibit_MomentumTable.ino
+++ Exhibit_MomentumTable.ino
@@ -3,27 +3,31 @@
    control from AutomationDirect. Control exists through contact 
    closure only at this time. 
 */
-#define DESIREDRPM_TABLE    60      // desired RPM on output wheel
-#define DESIREDRPM_MOTOR    600     // desired motor RPM (this may be superfulous) 
-#define ALARMTIME           3000    // How long an alarm has to be present before alarm 
-#define ROTATIONVARIANCE    50      // Variance in RPM allowable before alarm condition 
-#define TRANSMISSIONRATIO   10      // Tranmission drive ratio n:1
-#define NUMBEROFRESTARTS    10      // How many restarts to try after a stopped condition
-#define STARTTIME           10000   // Expected ramp up time, in MS
-#define STOPTIME            10000   // Expected ramp down time, in MS
+#define DESIREDRPM_TABLE    60      //desired RPM on output wheel
+#define DESIREDRPM_MOTOR    600     //desired motor RPM (this may be superfulous) 
+#define ALARMTIME           3000    //How long alarm cond before alarm signal
+#define ROTATIONVARIANCE    50      //Variance in RPM before alarm condition 
+#define TRANSMISSIONRATIO   10      //Tranmission drive ratio n:1
+#define NUMBEROFRESTARTS    10      //#of restarts attempted after stopped state
+#define STARTTIME           10000   //Expected ramp up time, in MS
+#define STOPTIME            10000   //Expected ramp down time, in MS
  /////////
-#define STARTRELAYPIN       D1      // pin locations
-#define HALLSENSOR1PIN      5       // dual hall sensors to detect rotaion
+#define STARTRELAYPIN       D1      //pin locations
+#define HALLSENSOR1PIN      5       //dual hall sensors to detect rotaion
 #define HALLSENSOR2PIN      4
 
 /**************************************/
 /*            GLOBAL VARS              /
 /**************************************/
 
-uint16_t hallSensor1_count  = 0
-uint16_t hallSensor2_count  = 0
+uint16_t hallSensor1_count  = 0 ;
+uint16_t hallSensor2_count  = 0 ;
 
 
+
+/**************************************/
+/*               SETUP                 /
+/**************************************/
 void setup()
 {
 pinMode(STARTRELAYPIN, OUTPUT);
@@ -33,15 +37,45 @@ pinMode(HALLSENSOR2PIN, INPUT);
 
 }
 
+
+/**************************************/
+/*                LOOP                 /
+/**************************************/
 void loop()
 {
+//wait for mqtt commands
+//send out mqtt status
+sensorPulseCount(); // returns formatted RPM number
+discSlipCheck();
+//if commanded != actual caution torque slip
+//if commanded != actual over time, report full error
+
 }
 
 
-
+/**************************************/
+/*             FUNCTIONS               /
+/**************************************/
 void sensorPulseCount(){}
+//current rotation value xx
+// report back RPM
 
 void motionControlStart(){}
+//enable start relay
+//check for rotation within x seconds
+//rotation state yes
+//current rotation value xx
+//if rotation doesn't start, report error
 
 void motiionControlStop(){}
+//disable start relay
+//check for stopped rotation within x seconds
+//rotation state no
+//current rotation value 00
+//if rotation doesn't stop, report error
 
+void discSlipCheck()
+{// check disc for slip condition
+//if below certain rotatation for x seconds, motiionControlStop
+
+}
