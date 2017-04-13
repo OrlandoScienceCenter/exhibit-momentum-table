ad95aed2763c7c2f078e544ca55a93cb5ecbd6c1
Michael King <techguy.mking@gmail.com>
2017-04-13 12:44:59 -0400

    filled out a lot of the functions

--- Exhibit_MomentumTable.ino
+++ Exhibit_MomentumTable.ino
@@ -11,17 +11,18 @@
 #include "Secrets.h"
 
 //////
-#define DESIREDRPM_TABLE    60      //desired RPM on output wheel
+//#define DESIREDRPM_TABLE    60      //desired RPM on output wheel
 #define DESIREDRPM_MOTOR    600     //desired motor RPM (this may be superfulous) 
-#define ALARMTIME           3000    //How long alarm cond before alarm signal
+#define DISCSLIPCOUNT       250     //How long alarm cond before alarm signal
+#define LOCKEDDISCRPM       10      //RPM to consider as "disc stopped"
 #define ROTATIONVARIANCE    50      //Variance in RPM before alarm condition 
-#define TRANSMISSIONRATIO   10      //Tranmission drive ratio n:1
-#define NUMBEROFRESTARTS    10      //#of restarts attempted after stopped state
+//#define TRANSMISSIONRATIO   10      //Tranmission drive ratio n:1
+#define NUMBEROFRESTARTS    5      //#of restarts attempted after stopped state
 #define STARTTIME           10000   //Expected ramp up time, in MS
 #define STOPTIME            10000   //Expected ramp down time, in MS
  /////////
 #define STARTRELAYPIN       1      //pin locations
-#define HALLSENSORPIN      5       //hall sensor to detect rotaion
+#define HALLSENSORPIN       5       //hall sensor to detect rotaion
 #define SECONDS             1000    //ms in seconds 
 
 WiFiClient espClient;
@@ -33,26 +34,26 @@ PubSubClient client(espClient);
 /*            GLOBAL VARS              /
 /**************************************/
 
-uint16_t hallSensor1_count  = 0 ;
-uint16_t hallSensor2_count  = 0 ;
-volatile uint8_t pulseCount = 0 ;
-uint8_t rpm                 = 0 ;
-unsigned long timeOld       = 0 ;
+uint16_t hallSensor1_count  = 0;
+uint16_t hallSensor2_count  = 0;
+volatile uint8_t pulseCount = 0;
+uint8_t rpm                 = 0;
+unsigned long timeOld       = 0;
 //
-unsigned long now = 0 ;
-unsigned long prevMillis = 0 ;
-unsigned long OTAUntilMillis = 0 ;
+unsigned long now           = 0;
+unsigned long prevMillis    = 0;
+unsigned long OTAUntilMillis = 0;
+uint8_t discSlipPrecond    = 0;
+uint8_t restartCount        = 0;
 char msg[50];                   
 
 /**************************************/
 /*               SETUP                 /
 /**************************************/
-void setup()
-{
+void setup(){
 pinMode(STARTRELAYPIN, OUTPUT);
-pinMode(HALLSENSOR1PIN, INPUT);
+pinMode(HALLSENSORPIN, INPUT);
 attachInterrupt(digitalPinToInterrupt(HALLSENSORPIN), sensorPulseCount, RISING);
-
 client.setServer(mqtt_server, 1883);
 client.setCallback(callback);
 }
@@ -61,12 +62,12 @@ client.setCallback(callback);
 /**************************************/
 /*                LOOP                 /
 /**************************************/
-void loop()
-{
+void loop(){
 //wait for mqtt commands
 //send out mqtt status
 calculateRPM();
 discSlipCheck();
+lockedRotorCheck();
 //if commanded != actual caution torque slip
 //if commanded != actual over time, report full error
 now = millis();
@@ -97,12 +98,26 @@ if (millis() - prevMillis == 1000){
     }
   }
 
-void motionControlStart(){}
-//enable start relay
-//check for rotation within x seconds
-//rotation state yes
-//current rotation value xx
-//if rotation doesn't start, report error
+void motionControlStart(){
+    if (restartCount < NUMBEROFRESTARTS){
+        digitalWrite(STARTRELAYPIN, HIGH); // enable start relay if not above fail counter
+        delay(2000); // wait for 2 second for a rotation 
+        if (pulseCount > 1){               // if no rotation detected in interrupt
+            digitalWrite(STARTRELAYPIN, LOW); // turn off motor becuase of no rotation
+            restartCount++;
+            delay (2000); // delay 2 seconds before attempting restart
+            pulseCount = 0; // reset pulse count 
+            motionControlStart();
+            }
+        else {
+        restartCount = 0; // set restart counter to 0, since we're started
+        }
+    }    
+    if (restartCount >= NUMBEROFRESTARTS) {
+        //MQTT "Disc Start / Restart Failed ---- SYSTEM HALTED"
+        allStop();   
+    }
+}
 
 void motiionControlStop(){}
 //disable start relay
@@ -111,8 +126,31 @@ void motiionControlStop(){}
 //current rotation value 00
 //if rotation doesn't stop, report error
 
-void discSlipCheck()
-{// check disc for slip condition
-//if below certain rotatation for x seconds, motiionControlStop
-
+void discSlipCheck(){
+    if (rpm < (DESIREDRPM_TABLE - ROTATIONVARIANCE)){
+        discSlipPrecond++;
+        }
+    else
+        if (discSlipPrecond > 0){
+        discSlipPrecond--;
+        }
+    if (discSlipPrecond > DISCSLIPCOUNT){
+    //SEND MQTT MESSAGE HERE
+    // "Caution! - Disc Slip Detected"
+    //
+    }
 }
+
+void lockedRotorCheck(){
+    if (rpm <= LOCKEDDISCRPM){
+        digitalWrite(STARTRELAYPIN, LOW); // immediately shut down if RPM lowByte
+        }                                 // disc is stuck or stopped 
+        //SEND MQTT MESSAGE HERE "WARNING! Disc has stopped rotation"
+              
+}
+
+void allStop(){
+    digitalWrite(STARTRELAYPIN, LOW);
+    // wait here until MQTT command to restart or system power cycle
+    //motionControlStart();
+    
