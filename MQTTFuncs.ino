db3299c50df9e099668b6779cb3280c0b3544723
12voltman <schuhplattler42@gmail.com>
2017-08-26 12:40:12 -0400

    Added wifi and OTA
    Added wifi connect and ota functions from generic automation sketch

--- MQTTFuncs.ino
+++ MQTTFuncs.ino
@@ -89,3 +89,34 @@ void reconnect() {
     }
   }
 }
+void wifiSetupOTA(){
+  ArduinoOTA.setHostname(OTA_HOSTNAME);
+ 
+  ArduinoOTA.onStart([]() {});
+  ArduinoOTA.onEnd([]() {});
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.print(F("Err"));
+  });
+  ArduinoOTA.begin();
+}
+
+void wifiSetup(){
+  WiFi.mode(WIFI_STA);
+  Serial.println();
+  Serial.print(F("Connecting to "));
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(F("."));
+  }
+
+  Serial.println();
+  Serial.println(F("WiFi connected"));
+  Serial.println(F("IP address: "));
+  Serial.println(WiFi.localIP());
+}
