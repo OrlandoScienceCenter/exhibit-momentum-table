fe233a3168643ec1c68b1977599a253014fdb3d9
12voltman <schuhplattler42@gmail.com>
2017-08-26 10:56:48 -0400

    Update Secrets.h

--- Secrets.h
+++ Secrets.h
@@ -4,6 +4,9 @@
 // Number of seconds on power applied before device actually turns on. For power outages, restarts, etc...
 #define STARTUP_DELAY_SECONDS 30
 
-#define ssid "Exhibits_F1_PZ"
+//#define ssid "Exhibits_F1_PZ"
+//#define ssid "Exhibits_F2_OG"
+#define ssid "Exhibits_F3_EI"
+//#define ssid "Exhibits_F4_OP"
 #define password "blowstuffup47"
 #define mqtt_server "192.168.1.254" // Or whatever
